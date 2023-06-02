478700f150e64fe45c84b4bb2b7c37b25d568d3a
josh <jgcarwayfc@gmail.com>
2023-06-02 18:46:09 -0500

    cambio del logo, creacion del footer

--- footer.php
+++ footer.php
@@ -1,8 +1,22 @@
 </section>
     <section class="footer">
-        
+        <div></div>
+        <div class="footer-logo">
+            <img src="img/logo.png" alt="" srcset="">
+        </div>
+        <div class="footer-phone">
+            <p><i class="fa-solid fa-phone"></i> +51 912 841 899</p>
+            <p><i class="fa-solid fa-phone"></i> +51 912 841 899</p>
+        </div>
+        <div class="footer-gmail">
+            <p><i class="fa-regular fa-envelope"></i> plankservice@gmail.com</p>
+            <p><i class="fa-regular fa-map"></i> HUÀNUCO</p>
+        </div>
+        <div></div>
     </section>
-
+    <div class="footer-copy">
+            <p><i class="fa-regular fa-copyright"></i> 2023 PLANK S.T.</p>
+    </div>
 </body>
 
 </html>
