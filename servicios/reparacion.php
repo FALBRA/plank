fa49635f9d151c99dd3328ebc13ca8db1acc210a
josh <jgcarwayfc@gmail.com>
2023-06-14 13:11:39 -0500

    Agregado encabezado de pa pag de reparacion con su css

--- servicios/reparacion.php
+++ servicios/reparacion.php
@@ -1,7 +1,42 @@
 <?php
 include("header-servicio.php");
 ?>
-
+<link rel="stylesheet" href="../css/style-reparacion.css">
+<section class="encabezado-reparacion">
+    <div class="encabezado">
+        <h1>Reparacion</h1>
+        <hr class="linea_separacion-reparacion">
+        <p>Reparación confiable y a domicilio de dispositivos tecnológicos. ¡Confíe en nosotros para restaurar su tecnología al máximo!</p>
+    </div>
+    <div class="bot">
+        <div class="bot-reparacion">
+            <img src="../img/servicios/reparacion/botReparacion.png" alt="">
+        </div>
+        <div class="descripcion-reparacion">
+            <p>Entendemos la importancia de mantener tus dispositivos en óptimas condiciones y que tu comodidad son importantes. Es por eso que ofrecemos un servicio de reparación a domicilio. ¡estamos aquí para ayudarte!</p>
+            <div class="emotes-reparacion">
+                <div class="compu">
+                    <i class="fa-solid fa-computer"></i>
+                    <p>Computadoras</p>
+                </div>
+                <div class="lap">
+                    <i class="fa-solid fa-laptop"></i>
+                    <p>Laptops</p>
+                </div>
+                <div class="cel">
+                    <i class="fa-solid fa-mobile-alt"></i>
+                    <p>Celulares</p>
+                </div>
+                <div class="otro">
+                    <i class="fa-solid fa-ellipsis-h"></i>
+                    <p>Otros</p>
+                </div>
+            </div>
+            <p>Reparamos desde los problemas más comunes hasta las situaciones más complejas.</p>
+            <p>Aquí hay un vistazo a las reparaciones que ofrecemos:</p>
+        </div>
+    </div>
+</section>
 
 <?php
 include("footer-servicio.php");
