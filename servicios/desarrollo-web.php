cb3ee1b234008c947ffb9151a41fe0c633460722
josh <jgcarwayfc@gmail.com>
2023-06-29 10:41:45 -0500

    Agregado todos los paquetes de desarrollo web

--- servicios/desarrollo-web.php
+++ servicios/desarrollo-web.php
@@ -26,6 +26,160 @@ include("header-servicio.php");
             <h2>Desde $500 a $1,500</h2>
         </div>
     </div>
+    <div class="desarrollo-web-basico">
+        <div class="desarrollo-web-nombre">
+            <h1>Desarrollo web y programación:</h1>
+        </div>
+        <div class="desarrollo-web-imagen">
+            <img src="../img/servicios/servicio.jpg" alt="">
+        </div>
+        <div class="desarrollo-web-descripcion">
+            <h2>Desarrollo de la estructura técnica del sitio web utilizando lenguajes de programación y tecnologías web.</h2>
+        </div>
+        <div class="desarrollo-web-beneficio">
+            <h2 class="negrita">Beneficios clave:</h2>
+            <h2>Funcionalidades personalizadas, interactividad.</h2>
+        </div>
+        <div class="desarrollo-web-precio">
+            <h2>Desde $1,000 a $3,000</h2>
+        </div>
+    </div>
+    <div class="desarrollo-web-basico">
+        <div class="desarrollo-web-nombre">
+            <h1>Diseño responsivo:</h1>
+        </div>
+        <div class="desarrollo-web-imagen">
+            <img src="../img/servicios/servicio.jpg" alt="">
+        </div>
+        <div class="desarrollo-web-descripcion">
+            <h2>Adaptación del diseño y la funcionalidad del sitio web para que se vea y funcione correctamente en dispositivos móviles y tablets.</h2>
+        </div>
+        <div class="desarrollo-web-beneficio">
+            <h2 class="negrita">Beneficios clave:</h2>
+            <h2>Experiencia de usuario consistente en todos los dispositivos, mayor alcance de audiencia.</h2>
+        </div>
+        <div class="desarrollo-web-precio">
+            <h2>Desde $500 a $1,500</h2>
+        </div>
+    </div>
+    <div class="desarrollo-web-basico">
+        <div class="desarrollo-web-nombre">
+            <h1>Integración de CMS:</h1>
+        </div>
+        <div class="desarrollo-web-imagen">
+            <img src="../img/servicios/servicio.jpg" alt="">
+        </div>
+        <div class="desarrollo-web-descripcion">
+            <h2>Implementación de un CMS para permitir al cliente administrar y actualizar fácilmente el contenido del sitio web.</h2>
+        </div>
+        <div class="desarrollo-web-beneficio">
+            <h2 class="negrita">Beneficios clave:</h2>
+            <h2>Autonomía para el cliente, actualizaciones sencillas, mayor flexibilidad en la gestión de contenido.</h2>
+        </div>
+        <div class="desarrollo-web-precio">
+            <h2>Desde $800 a $2,000</h2>
+        </div>
+    </div>
+    <div class="desarrollo-web-basico">
+        <div class="desarrollo-web-nombre">
+            <h1>Optimización de velocidad y rendimiento:</h1>
+        </div>
+        <div class="desarrollo-web-imagen">
+            <img src="../img/servicios/servicio.jpg" alt="">
+        </div>
+        <div class="desarrollo-web-descripcion">
+            <h2>Optimización de imágenes, compresión de archivos y ajuste de configuraciones para un mejor rendimiento.</h2>
+        </div>
+        <div class="desarrollo-web-beneficio">
+            <h2 class="negrita">Beneficios clave:</h2>
+            <h2>Experiencia de usuario mejorada, menor tasa de rebote.</h2>
+        </div>
+        <div class="desarrollo-web-precio">
+            <h2>Desde $300 a $800.</h2>
+        </div>
+    </div>
+    <div class="desarrollo-web-basico">
+        <div class="desarrollo-web-nombre">
+            <h1>Seguridad y protección contra ataques:</h1>
+        </div>
+        <div class="desarrollo-web-imagen">
+            <img src="../img/servicios/servicio.jpg" alt="">
+        </div>
+        <div class="desarrollo-web-descripcion">
+            <h2>Implementación de medidas de seguridad, como certificados SSL, protección contra ataques de hackers y actualizaciones de seguridad regulares.</h2>
+        </div>
+        <div class="desarrollo-web-beneficio">
+            <h2 class="negrita">Beneficios clave:</h2>
+            <h2>Protección de datos, confianza del cliente, cumplimiento de normas de seguridad en línea.</h2>
+        </div>
+        <div class="desarrollo-web-precio">
+            <h2>Desde $500 a $1,500.</h2>
+        </div>
+    </div>
+    <div class="desarrollo-web-basico">
+        <div class="desarrollo-web-nombre">
+            <h1>Integración de comercio electrónico:</h1>
+        </div>
+        <div class="desarrollo-web-imagen">
+            <img src="../img/servicios/servicio.jpg" alt="">
+        </div>
+        <div class="desarrollo-web-descripcion">
+            <h2>Configuración de una tienda en línea con carrito de compras, pasarelas de pago y gestión de inventario.</h2>
+        </div>
+        <div class="desarrollo-web-beneficio">
+            <h2 class="negrita">Beneficios clave:</h2>
+            <h2>Oportunidad de ventas en línea, ampliación del alcance de mercado, automatización de procesos comerciales.</h2>
+        </div>
+        <div class="desarrollo-web-precio">
+            <h2>Desde $1,500 a $5,000</h2>
+        </div>
+    </div>
+    <div class="desarrollo-web-basico">
+        <div class="desarrollo-web-nombre">
+            <h1>SEO básico:</h1>
+        </div>
+        <div class="desarrollo-web-imagen">
+            <img src="../img/servicios/servicio.jpg" alt="">
+        </div>
+        <div class="desarrollo-web-descripcion">
+            <h2>Optimización del sitio web para mejorar su visibilidad en los motores de búsqueda, incluyendo investigación de palabras clave, etiquetas meta y optimización de contenido.</h2>
+        </div>
+        <div class="desarrollo-web-beneficio">
+            <h2 class="negrita">Beneficios clave:</h2>
+            <h2>Mejor posicionamiento en motores de búsqueda, aumento del tráfico orgánico, generación de clientes potenciales.</h2>
+        </div>
+        <div class="desarrollo-web-precio">
+            <h2>Desde $500 a $1,500.</h2>
+        </div>
+    </div>
+</section>
+<section class="trabajos-anteriores">
+    <h2>Trabajos Anteriores</h2>
+    <div class="carrusel">
+        <div class="anterior">
+
+        </div>
+        
+        <div class="siguiente">
+
+        </div>
+    </div>
+</section>
+<section class="comentarios">
+    <h2>COMENTARIOS DE CLIENTES</h2>
+    <div class="usuario">
+        <div class="foto">
+            <div class="circulo foto"><img src="../img/white.png" alt=""></div>
+        </div>
+        <div class="usuario-comentario">
+            <div class="nombre-usuario">
+                <p>nombre de cliente</p>
+            </div>
+            <div class="comentario">
+                <p>comentario,comentario,comentario,comentario,comentario,comentario,comentario,comentario,</p>
+            </div>
+        </div>
+    </div>
 </section>
 <?php
 include("footer-servicio.php");
