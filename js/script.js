537ade4ba2283affc592d81b35ce3c5b619350b9
alessandro <adim180716@gmail.com>
2023-06-29 14:57:25 -0500

    Agregadas las funcionalidades de la pag nosotros / faltan detalles

--- js/script.js
+++
@@ -1,7 +0,0 @@
-const myCarousel = new Carousel({
-    container: document.querySelector('.slider'),
-    items: document.querySelectorAll('.slider__item'),
-    displayControls: true,
-    controlsContainer: document.querySelector('.slider__controls'),
-    textControls: ["<i class='fas fa-chevron-left'></i>", "<i class='fas fa-chevron-right'></i>"]
-});
