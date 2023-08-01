ce232ceaecb8ae7b280f8297a5837a84b4fbae73
josh <jgcarwayfc@gmail.com>
2023-07-31 21:41:42 -0500

    intentado agregar pagina de cliente

---
+++ cliente.php
@@ -0,0 +1,33 @@
+<?php
+include("header.php");
+?>
+<link href="css/style-cliente.css" rel="stylesheet" />
+<section class="encabezado-cliente">
+    <div class="encabezado-cleinte-container">
+        <div class="perfil">
+            <div class="foto">
+                <img src="img/white.png" alt="white">
+            </div>
+            <div class="nickname">
+                <h1>nickname</h1>
+            </div>
+        </div>
+        <div class="actividad-option">
+            <input type="checkbox" id="btn-option">
+            <label for="btn-option" class="icon-option" id="option-label-cliente"><i class="fa-solid fa-ellipsis-vertical"></i></label>
+            <div class="actividad">
+                <h2>TU ACTIVIDAD</h2>
+            </div>
+            <div class="option">
+                <h2>OPCIONES</h2>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="">
+    
+</section>
+
+<?php
+include("footer.php");
+?>
