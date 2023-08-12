031ca3dee35cad34a7d52ffce8ab567d9091f1e6
josh <jgcarwayfc@gmail.com>
2023-08-12 18:18:05 -0500

    agregado la pagina del admiistrador

---
+++ tablas-servicios-administrador/tabla-instalacion.php
@@ -0,0 +1,26 @@
+<table class="tabla-instalacion">
+    <tr>
+        <th>Nª</th>
+        <th>Fecha</th>
+        <th>Cliente</th>
+        <th>Contacto</th>
+        <th>Direccion de la Instalacion</th>
+        <th>equipo / programa</th>
+        <th>marca / modelo</th>
+        <th>Descripcion de la Instalacion</th>
+        <th>Metodo Pago</th>
+        <th>Estado</th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>12/12/2023</td>
+        <td>Juan Perez Inga</td>
+        <td>123 456 789</td>
+        <td>Jr. Damaso Beraun 128</td>
+        <td>Camaras de seguridad</td>
+        <td>Security</td>
+        <td>ksdh gsld kfsld kfkd jflskdh fsknvi ewodn ksldknv jdskldjv nsldkfjei ekfn wlekfnslkd nsekfhsdkfh sdifhdklskdfj sdkfjsldkfjsghsldk slkdfhsldkfsld slkdfdlskfjsldkj sdfsdfdsf</td>
+        <td>transferencia Bancaria</td>
+        <td>1<i class="fa-brands fa-stack-overflow"></i></td>
+    </tr>
+</table>
